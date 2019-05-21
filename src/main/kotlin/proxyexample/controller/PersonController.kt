2684b822356ffffcc58b7b47f92764476ffc49f0
Miguel Ángel Aguilar Campos <miaguicam@gmail.com>
2019-05-21 09:38:56 +0200

    added server

--- src/main/kotlin/proxyexample/controller/PersonController.kt
+++ src/main/kotlin/proxyexample/controller/PersonController.kt
@@ -1,2 +1,14 @@
 package proxyexample.controller
 
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+
+
+@Controller("/persons")
+class PersonController {
+
+    @Get("/")
+    fun show(): String {
+        return "person"
+    }
+}
